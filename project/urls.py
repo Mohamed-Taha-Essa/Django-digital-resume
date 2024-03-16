6e7fc06fdb86f1f8345789e27736032edcf9dd95
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-03-17 01:31:02 +0200

    show ertificates in index.html

--- project/urls.py
+++ project/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('resume.urls')),
+    path('home/', include('resume.urls')),
 
 ]
 
