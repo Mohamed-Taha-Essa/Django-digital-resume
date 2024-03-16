12ad4f94f899b0334844df626f8bc74e99893b30
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-03-16 11:20:59 +0200

    add form to contact form

--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . models import UserProfile ,Skill,ContactProfile,Certificate\
+                    ,Portfolio,Blog,Testimonial,Media
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Skill)
+admin.site.register(ContactProfile)
+admin.site.register(Certificate)
+admin.site.register(Portfolio)
+admin.site.register(Blog)
+admin.site.register(Testimonial)
+admin.site.register(Media)
--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.contrib import messages
+from .models import (
+		UserProfile,
+		Blog,
+		Portfolio,
+		Testimonial,
+		Certificate
+	)
+
+from django.views import generic
+
+
+from . forms import ContactForm
+
+
+class IndexView(generic.TemplateView):
+	template_name = "resume/index.html"
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		
+		testimonials = Testimonial.objects.filter(is_active=True)
+		certificates = Certificate.objects.filter(is_active=True)
+		blogs = Blog.objects.filter(is_active=True)
+		portfolio = Portfolio.objects.filter(is_active=True)
+		
+		context["testimonials"] = testimonials
+		context["certificates"] = certificates
+		context["blogs"] = blogs
+		context["portfolio"] = portfolio
+		return context
